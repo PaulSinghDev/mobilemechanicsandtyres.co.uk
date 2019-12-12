82ddb9e6741fdb88e9b15c6fbf5c80b0d08e4c13
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2019-12-12 18:21:49 +0000

    Added referenced to the two images which are used within the header to allow webpack to bundle them

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,8 @@
 import '../css/main.css';
 import '../fonts/fa/css/all.min.css';
+import '../img/logo-full-dark.svg';
+import '../img/car-repair.jpg';
+
 console.log('Yes');
 
 module.hot.accept((err) => console.log(err))
