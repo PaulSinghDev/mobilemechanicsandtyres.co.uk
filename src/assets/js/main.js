f4499255848156c821f578f83a22e3a4301fccd8
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2019-12-14 10:23:06 +0000

    Added reference to workshop-tools.jpg

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,7 @@ import '../css/main.css';
 import '@fortawesome/fontawesome-free/js/all'
 import '../img/logo-full-dark.svg';
 import '../img/car-repair.jpg';
+import '../img/workshop-tools.jpg';
 
 console.log('Hello');
 
