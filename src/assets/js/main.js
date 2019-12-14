c8a1a4185e2f7988c7e1f155d217e149794fffcc
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2019-12-14 20:38:31 +0000

    Added references to new images

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,21 @@
 import '../css/main.css';
 import '@fortawesome/fontawesome-free/js/all'
 import '../img/logo-full-dark.svg';
+import '../img/logo-dark.svg';
 import '../img/car-repair.jpg';
 import '../img/workshop-tools.jpg';
+import '../img/mot-tile.jpg';
+import '../img/collection-tile.jpg';
+import '../img/diagnosis-tile.jpg';
+import '../img/tyre-tile.jpg';
+import '../img/aircon-tile.jpg';
+import '../img/assembly-tile.jpg';
+import '../img/brembo-logo.jpg';
+import '../img/shell-helix-logo.png';
+import '../img/fag-logo.png';
+import '../img/ina-logo.gif';
+import '../img/luk-logo.jpg';
+import '../img/varta-logo.png';
 
 console.log('Hello');
 
