36fd395d7df1196e854b96c8321c870fd9785dba
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2019-12-18 00:28:46 +0000

    Removed references to images - Added reference to scss file instead of css

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,6 @@
-import '../css/main.css';
+import '../scss/main.scss';
 import '@fortawesome/fontawesome-free/js/all';
-import '../img/logo-full-dark.svg';
-import '../img/logo-dark.svg';
-import '../img/car-repair.jpg';
-import '../img/workshop-tools.jpg';
-import '../img/mot-tile.jpg';
-import '../img/collection-tile.jpg';
-import '../img/diagnosis-tile.jpg';
-import '../img/tyre-tile.jpg';
-import '../img/aircon-tile.jpg';
-import '../img/assembly-tile.jpg';
-import '../img/brembo-logo.jpg';
-import '../img/shell-helix-logo.png';
-import '../img/fag-logo.png';
-import '../img/ina-logo.gif';
-import '../img/luk-logo.jpg';
-import '../img/varta-logo.png';
 
-console.log('Hello');
+console.log('He');
 
 module.hot.accept((err) => console.log(err))
