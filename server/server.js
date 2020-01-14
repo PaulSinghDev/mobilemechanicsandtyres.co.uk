020e6fc4ab1b0f4b0bfde38b192c54112696476e
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-01-14 19:43:09 +0000

    Added 404 middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const webpack = require('webpack');
+const path = require('path');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('../webpack.dev');
@@ -12,5 +13,11 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 
 app.use(webpackHotMiddleware(compiler));
+
+app.use(function (req, res, next) {
+    compiler.outputFileSystem.readFile(path.join(__dirname, '../dist/','404.html'), (err, result) => {
+        res.status(404).set('content-type', 'text/html').send(result).end();
+    })
+})
 
 app.listen(3000, () => console.log('hey, we\'re connected on port 3000'));
