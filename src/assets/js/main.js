2df86b03c4c93f050e3a6cd082543d21a516fee3
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-01-14 14:32:01 +0000

    Import observers

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,7 @@ import '../scss/main.scss';
 import './navControl';
 import './tileFlipper';
 import './contact';
+import './observers';
 
 import { library, dom} from '@fortawesome/fontawesome-svg-core';
 import {faStar, faBars, faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons';
