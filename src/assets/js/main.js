7d1d203fb935563773b288f39a739d5123587b3a
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-01-15 12:08:34 +0000

    - Added event listener to #terms and #privacy - Event listeners create an ms-modal element and set the templateID attribute to allow for dynamic content building - Added import call to ms-modal custom element class

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,11 +3,36 @@ import './navControl';
 import './tileFlipper';
 import './contact';
 import './observers';
+import '../web-components/ms-modal';
 
-import { library, dom} from '@fortawesome/fontawesome-svg-core';
-import {faStar, faBars, faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons';
+import {
+    library,
+    dom
+} from '@fortawesome/fontawesome-svg-core';
+import {
+    faStar,
+    faBars,
+    faAngleDoubleRight
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    msQuery,
+    msCreate
+} from 'making-stuffs-queries';
 
 library.add(faStar, faBars, faAngleDoubleRight);
 dom.i2svg();
+
+
+msQuery('#terms').addEventListener('click', (e) => {
+    e.stopPropagation();
+    const modal = msCreate('ms-modal', { open: '', templateID: 'termsTemplate' })
+    msQuery('body').append(modal);
+});
+
+msQuery('#privacy').addEventListener('click', (e) => {
+    e.stopPropagation();
+    const modal = msCreate('ms-modal', { open: '', templateID: 'privacyTemplate' })
+    msQuery('body').append(modal);
+});
 
 module.hot.accept((err) => console.log(err));
