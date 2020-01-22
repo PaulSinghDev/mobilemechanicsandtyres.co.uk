1cd761ab9a85b44e8166cd53cefbafb62a2df82c
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-01-22 16:50:37 +0000

    Created an suth server for Google reCAPTCHA v3

---
+++ server/auth-server.js
@@ -0,0 +1,49 @@
+require('dotenv').config({path: './.env'});
+require('express-async-await');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const https = require('https');
+const app = express();
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"]
+    }
+}));
+app.use(cors());
+app.use(express.json());
+
+const {
+    CAPTCHA_URL,
+    CAPTCHA_PORT,
+    CAPTCHA_SECRET,
+    SSL_KEY,
+    SSL_CERT,
+    SSL_PW
+} = process.env;
+
+app.post('/auth', async (req, res) => {
+    const reply = await fetch(CAPTCHA_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `secret=${CAPTCHA_SECRET}&response=${req.body.token}`
+    });
+    const data = await reply.json();
+    res.send(data);
+});
+
+const sslOptions = {
+    key: fs.readFileSync(SSL_KEY),
+    cert: fs.readFileSync(SSL_CERT),
+    passphrase: SSL_PW
+}
+
+const httpsServer = https.createServer(sslOptions, app);
+
+const listener = httpsServer.listen(CAPTCHA_PORT || 5000, () => console.log('Connected on port ' + listener.address().port));
