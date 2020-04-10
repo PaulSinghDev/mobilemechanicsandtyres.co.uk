844dbf8620facbb40c8f3376e8850a792ff9d25f
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-04-10 21:50:59 +0530

    - Added missing semi-colons

--- src/assets/js/observers.js
+++ src/assets/js/observers.js
@@ -9,7 +9,7 @@ let options = {
     threshold: 0.05,
     margin: '50px',
     rot: null
-}
+};
 
 const callback = (entries) => {
     entries.forEach(entry => {
@@ -29,7 +29,7 @@ const callback = (entries) => {
             }
         }
     });
-}
+};
 
 const addMap = () => {
     const placeHolder = msQuery('.contact-info');
@@ -39,21 +39,20 @@ const addMap = () => {
             src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2475.638917579317!2d-0.17216638471171977!3d51.64813750822486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876194b8eedcd0d%3A0xb869a879f52e9c43!2smobile%20mechanic%20and%20tyres!5e0!3m2!1sen!2suk!4v1576429278378!5m2!1sen!2suk", 
             width: "100%",
             height: "100%",
-            frameborder: "0",
             style: "border:0;",
             allowfullscreen:""
-        }
+        };
         const mapiFrame = msCreate('iframe', iFrameOptions);
         placeHolder.appendChild(mapiFrame);
         loader.remove();
     } else return;
-}
+};
 
 const observer = new IntersectionObserver(callback, options);
 
 if(!header.classList.contains('home-header')) {
     const footer = msQuery('footer');
-    observer.observe(footer)
+    observer.observe(footer);
 } else {
     observer.observe(main);
     observer.observe(contact);
