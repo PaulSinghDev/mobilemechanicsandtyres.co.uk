fecd02458b3fb2d9e23f8a4ab31d61071b25723c
Paul Singh <hey@paulsingh.dev>
2023-08-07 20:00:46 +0100

    :wastebasket: Wipe oold app, install vitw

--- server/utils/logger.js
+++
@@ -1,9 +0,0 @@
-const info = (...params) => console.log(...params);
-const warn = (...params) => console.warn(...params);
-const error = (...params) => console.error(...params);
-
-module.exports = {
-  info,
-  error,
-  warn,
-};
